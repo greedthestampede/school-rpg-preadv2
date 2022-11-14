024f7b10bd72cc2c77376609183248c852700600
greedthestampede <roburolireis@gmail.com>
2022-11-14 17:47:32 -0300

    Retirando aluna e acrescentando número

--- js/codes.js
+++ js/codes.js
@@ -9,7 +9,7 @@ async function preADV2() {
     const csvtext = await csv().fromString(text);
     main.innerHTML = '<h2>Heroes</h2>';
     const showMe = await csvtext.forEach(function(row) {
-        main.innerHTML += "<p class='heroes'> <b><i>" + row.PADVName + "</i></b> [" + row.PADVClass + "] (" + row.PADVRanking + ") " + row.PADVXP + "XP " + row.PADVFP + "FP </p>";      
+        main.innerHTML += "<p class='heroes'> "+ row.PADVNumber + ". " + " <b><i>" + row.PADVName + "</i></b> [" + row.PADVClass + "] (" + row.PADVRanking + ") " + row.PADVXP + "XP " + row.PADVFP + "FP </p>";      
     });
     return showMe;
 }
